a76871e8d31a5ae1b8d97108aa88581e4cd4a841
Hiếu <dinhhieunguyen2003@gmail.com>
2024-10-24 00:12:10 +0700

    add technical and project Menu + add some media queries

---
+++ GroupWeb/public/script.js
@@ -0,0 +1,19 @@
+function showSection(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => section.style.display = 'none');
+    
+    // Show the selected section
+    const sectionToShow = document.getElementById(sectionId);
+    sectionToShow.style.display = 'block';
+}
+function toggleContent(index) {
+    const content = document.getElementById('content-' + index);
+
+    // Toggle the visibility of the content
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
